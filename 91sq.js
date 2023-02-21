919cf896803f450b15438b0ebfef66c1184f483a
Zlivex <118098943+Zlivex@users.noreply.github.com>
2023-02-22 01:09:07 +0800

    Update 91sq.js

--- 91sq.js
+++ 91sq.js
@@ -2,7 +2,7 @@
 #!desc=91视频解锁会员
 [Script]
 http-response
-^http[s]?:\/\/.+((ppapi)|(17gwl)|(sgyrc)|(mnghx)|(czbt)|(jtshuzhiwa)).(info|cc|com)\/m_sns\/(video\/apply_play_permission|film)。*$
+^http[s]?:\/\/.+((ppapi)|(17gwl)|(sgyrc)|(mnghx)|(czbt)|(jtshuzhiwa)).(info|cc|com)\/m_sns\/(video\/apply_play_permission|film).*$
 require-body=1，script-path=https://raw.githubusercontent.com/WeiRen0/Scripts/main/91.js
 [MITM]
 hostname = *17gwl*,appapi*,*sgyrc*,*mnghx*,*czbt*,*jtshuzhiwa*,mnghx,czbt
