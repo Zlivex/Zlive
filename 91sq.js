d52933110d40ff4d0381061b2362d08745825d4b
Zlivex <118098943+Zlivex@users.noreply.github.com>
2023-02-22 01:12:04 +0800

    Update 91sq.js

--- 91sq.js
+++ 91sq.js
@@ -3,6 +3,6 @@
 [Script]
 http-response
 ^http[s]?:\/\/.+((ppapi)|(17gwl)|(sgyrc)|(mnghx)|(czbt)|(jtshuzhiwa)).(info|cc|com)\/m_sns\/(video\/apply_play_permission|film).*$
-require-body=1，script-path=https://raw.githubusercontent.com/WeiRen0/Scripts/main/91.js
+require-body=1,script-path=https://raw.githubusercontent.com/WeiRen0/Scripts/main/91.js
 [MITM]
 hostname = *17gwl*,appapi*,*sgyrc*,*mnghx*,*czbt*,*jtshuzhiwa*,mnghx,czbt
